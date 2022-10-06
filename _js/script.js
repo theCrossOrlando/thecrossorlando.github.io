5dfd0a125572b05f7f41c11119746f3bd3889b4a
Albert Volkman <albert.volkman@gmail.com>
2022-10-06 16:19:07 -0400

    Wait for images to load to execute masonry

--- _js/script.js
+++ _js/script.js
@@ -1,3 +1,13 @@
-var msnry = new Masonry('#content .row', {
-  itemSelector: '.col'
+Promise.all(
+  Array.from(document.images)
+    .filter(img => !img.complete)
+    .map(img =>
+      new Promise(resolve => {
+        img.onload = img.onerror = resolve;
+      })
+    )
+).then(() => {
+  var msnry = new Masonry('#content .row', {
+    itemSelector: '.col',
+  });
 });
