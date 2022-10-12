956180dd0f116cb2541e8b39f58108f6f83ba815
Albert Volkman <albert.volkman@gmail.com>
2022-10-11 20:45:25 -0400

    Update gulp dev/build script

--- gulpfile.js
+++ gulpfile.js
@@ -12,54 +12,85 @@ import cssnano from 'cssnano';
 import removeComments from 'postcss-discard-comments';
 import { default as log } from 'fancy-log';
 
-// Compile SASS.
-export const css = () => gulp.src('./_css/style.scss')
+// Dev
+export const cssDev = () => gulp.src('./_css/style.scss')
     .pipe(sass({
       includePaths: [
         './_css',
         './node_modules/bootstrap/scss',
-        './node_modules/@fortawesome/fontawesome-free/scss',
+        './node_modules/@fortawesome/fontawesome-free/scss'
+      ],
+    })
+    .on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+
+export const cssWatch = () => gulp.watch('./_css/**/*.scss', cssDev);
+
+export const jsDev = () => gulp.src([
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './node_modules/masonry-layout/dist/masonry.pkgd.min.js',
+    './_js/script.js'
+  ])
+  .pipe(gulp.dest('js'));
+
+export const jsWatch = () => gulp.watch('./_js/**/*.js', jsDev);
+
+export const jekyllServe = () => {
+  const jekyll = spawn('bundle', ['exec', 'jekyll', 'serve']);
+
+  const jekyllLogger = function(buffer) {
+    buffer.toString()
+      .split(/\n/)
+      .forEach(function(message) {
+        log('Jekyll: ' + message);
+      });
+  };
+
+  jekyll.stdout.on('data', jekyllLogger);
+  jekyll.stderr.on('data', jekyllLogger);
+}
+
+export const dev = gulp.series(cssDev, gulp.parallel(jekyllServe, cssWatch));
+
+// Prod
+export const cssProd = () => gulp.src('./_css/style.scss')
+    .pipe(sass({
+      includePaths: [
+        './_css',
+        './node_modules/bootstrap/scss',
+        './node_modules/@fortawesome/fontawesome-free/scss'
       ],
     })
     .on('error', sass.logError))
     .pipe(postcss([purgecss({
         content: [
-          './_site/index.html',
-          './_site/care.html',
-          './_site/gatherings.html',
-          './_site/lyrics/index.html',
-          './_site/messages/**/*.html',
-          './_site/people.html',
-          './_site/values.html',
-          './_site/work.html',
-        ],
+          '_site/index.html',
+          '_site/care.html',
+          '_site/gatherings.html',
+          '_site/lyrics/index.html',
+          '_site/messages/**/*.html',
+          '_site/people.html',
+          '_site/values.html',
+          '_site/work.html',
+        ]
       }),
       removeComments({ removeAll: true }),
-      cssnano]))
+      cssnano()
+    ]))
     .pipe(gulp.dest('./_site/css'));
 
-export const cssWatch = () => gulp.watch('./_css/**/*.scss', css);
-
-// Compile JS.
-export const js = () => gulp.src([
+export const jsProd = () => gulp.src([
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
     './node_modules/masonry-layout/dist/masonry.pkgd.min.js',
     './_js/script.js'
   ])
-    .pipe(concat('script.js'))
     .pipe(terser({
       toplevel: true,
       format: {
         comments: false
       }
     }))
-    .pipe(gulp.dest('./_site/js'));
-
-export const jsWatch = () => gulp.watch('./_js/**/*.js', ['js']);
-
-export const htmlMinify = () => gulp.src('_site/**/*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
-    .pipe(gulp.dest('_site'));
+    .pipe(gulp.dest('_site/js'));
 
 export const jekyll = (gulpCallback) => {
   const jekyll = spawn('bundle', ['exec', 'jekyll', 'build']);
@@ -78,24 +109,6 @@ export const jekyll = (gulpCallback) => {
   jekyll.on('exit', gulpCallback);
 }
 
-export const jekyllServe = () => {
-  const jekyll = spawn('bundle', ['exec', 'jekyll', 'serve']);
-
-  const jekyllLogger = function(buffer) {
-    buffer.toString()
-      .split(/\n/)
-      .forEach(function(message) {
-        log('Jekyll: ' + message);
-      });
-  };
-
-  jekyll.stdout.on('data', jekyllLogger);
-  jekyll.stderr.on('data', jekyllLogger);
-}
-
-//export const dev = gulp.series(css, js, jekyllServe, cssWatch, jsWatch);
-export const dev = gulp.parallel(css, cssWatch, jekyllServe);
-
-export const build = gulp.series(jekyll, css, js);
+export const build = gulp.series(jekyll, cssProd, jsProd);
 
 export default dev;
