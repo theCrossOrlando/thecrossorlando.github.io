bb198f6db2cf63fdd773ac0083a08cfce6838d05
Albert Volkman <albert.volkman@gmail.com>
2025-05-19 10:16:18 -0400

    Rework rss parser

--- _data/blogPosts.js
+++ _data/blogPosts.js
@@ -1,27 +1,18 @@
-import Fetch from "@11ty/eleventy-fetch";
-import xpath from 'xpath';
-import { DOMParser } from 'xmldom';
+import rssParser from 'rss-parser';
+
+const parser = new rssParser();
 
 export default async function () {
-  const select = xpath.useNamespaces({"xlink":"http://www.w3.org/1999/xlink", "ODM":"http://www.cdisc.org/ns/odm/v1.3", "def":"http://www.cdisc.org/ns/def/v2.0"});
 	const url = "https://tcandk.wordpress.com/feed/";
+  const feed = await parser.parseURL(url);
 
-	const xml = await Fetch(url, {
-		duration: "1d",
-		type: "xml",
-	});
-
-  const doc = new DOMParser().parseFromString(xml);
-  const blog_title = select('/rss/channel/title', doc)[0].firstChild.data;
-  const blog_posts = select('/rss//item', doc);
-
-  return blog_posts.map(p => ({
-    'blog': blog_title,
-    'title': p.getElementsByTagName('title')[0].childNodes[0].data,
-    'summary': p.getElementsByTagName('description')[0].childNodes[0].data,
-    'author': p.getElementsByTagName('dc:creator')[0].childNodes[0].data,
-    'link': p.getElementsByTagName('link')[0].childNodes[0].data,
-    'pubDate': p.getElementsByTagName('pubDate')[0].childNodes[0].data,
-    'timestamp': new Date(p.getElementsByTagName('pubDate')[0].childNodes[0].data),
+  return feed.items.map(p => ({
+    'blog': feed.title,
+    'title': p.title,
+    'summary': p.contentSnippet,
+    'author': p.creator,
+    'link': p.link,
+    'pubDate': p.pubDate,
+    'timestamp': p.isoDate,
   }));
 };
